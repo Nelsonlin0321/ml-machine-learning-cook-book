3458a31cc9fe7f475d3541f7be1a0c1c142feaf3
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-07-05 08:44:19 +0800

    add pytorch transformer

---
+++ behavior-sequence-transformer-recommender-with-pytorch-by-movie-rating-prediction copy/src/utils.py
@@ -0,0 +1,34 @@
+import pickle
+import os
+import torch
+import json
+
+def open_object(object_path):
+    with open(object_path, mode='rb') as f:
+        obj = pickle.load(f)
+
+    return obj
+
+
+def save_object(object_path, obj):
+    os.makedirs(os.path.dirname(object_path),exist_ok=True)
+    with open(object_path, mode='wb') as f:
+        pickle.dump(obj, f)
+
+
+def save_model(model, model_save_dir,step,model_metrics):
+    model_save_dir = os.path.join(model_save_dir,f"checkpoint-{step}")
+    model_name = "pytorch_model.pt"
+    train_state_name = "training_state.json"
+    os.makedirs(model_save_dir,exist_ok=True)
+    
+    model_path = os.path.join(model_save_dir,model_name)
+    train_state_path = os.path.join(model_save_dir,train_state_name)
+
+    torch.save(model.state_dict(),model_path)
+    
+    if model_metrics is not None:
+        with open(train_state_path,mode = 'w',encoding = 'utf-8') as f:
+            json.dump(model_metrics,f,indent=4)
+            
+    return model_path

fea3a8238cb37a1cb2d09473fbc57aa2945cfed7
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-07-09 02:14:18 +0800

    version 1 model

---
+++ behavior-sequence-transformer-recommender-with-pytorch-by-movie-rating-prediction copy/src/evaluation.py
@@ -0,0 +1,39 @@
+import torch
+from torch import nn
+from tqdm import tqdm
+
+def evaluate(model,dataset_loader,loss_func=nn.L1Loss()):
+    
+    model.eval()
+
+    prob_list= []
+    rating_list = []
+    eval_loss_list = []
+
+
+    # loss_func = nn.MSELoss()
+
+    pbar = tqdm(total = len(dataset_loader),desc = "",position=0, leave=True)
+
+    for inputs in dataset_loader:
+        with torch.no_grad():
+            probs = model(inputs)
+            ratings = inputs['target_rating'].view(-1,1)
+            
+            loss = loss_func(probs, ratings)
+            eval_loss_list.append(loss.item())
+
+            probs = probs.cpu().numpy().flatten().tolist()
+            prob_list.extend(probs)
+
+            ratings = ratings.numpy().flatten().tolist()
+            rating_list.extend(ratings)
+
+            pbar.update(1)
+
+    pbar.close()
+
+    eval_metrics = {}
+    eval_metrics['eval_loss']= sum(eval_loss_list)/len(eval_loss_list)
+    
+    return eval_metrics
